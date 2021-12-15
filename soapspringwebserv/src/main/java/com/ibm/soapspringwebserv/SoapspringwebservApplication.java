840bd668004c58d3c0aad1ca450ea04652e6dbd3
Roshni Kumari011 <roshnkum@in.ibm.com>
2021-12-15 21:09:10 +0530

    classfromwsdl

---
+++ soapspringwebserv/src/main/java/com/ibm/soapspringwebserv/SoapspringwebservApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.soapspringwebserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapspringwebservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapspringwebservApplication.class, args);
+	}
+
+}
---
+++ soapspringwebserv/src/test/java/com/ibm/soapspringwebserv/SoapspringwebservApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.soapspringwebserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapspringwebservApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
